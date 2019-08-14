e04a14c1f45bd70d114b2e76735c7f48b6887048
eshport <edgar@powow.ai>
2019-08-14 20:48:34 +0300

    commit msg cool

---
+++ static/js/OAuth.js
@@ -0,0 +1,30 @@
+//API_KEY = "AIzaSyDKHmG_iim8s1FRpgUxlG5JYtIH4R-8wSo";
+//CLIENT_ID = "138029415830-13pnjkeu8bs2536vhj3t1n1ngvsil6i5.apps.googleusercontent.com"
+API_KEY = "AIzaSyC5rmD2Gt1nNf-yX6T-IwrbFZUbZ1FU9ic";
+CLIENT_ID = "883952944674-fpfk1cca6q3etmjiafru9flq5c6dv066.apps.googleusercontent.com"
+
+
+function authenticate() {
+    return gapi.auth2.getAuthInstance()
+        .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
+            .then(() => console.log("Sign-in successful") ,
+                  (err) => console.error("Error signing in", err) )
+}
+
+function loadClient() {
+    gapi.client.setApiKey(API_KEY);
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then( () => console.log("Google API client loaded"),
+              (err) => console.error("Error loading Google API client", err) )
+            .then($("#auth").hide())
+                .then($("#request").show());
+}
+
+gapi.load("client:auth2", function() {
+    gapi.auth2.init({client_id: CLIENT_ID});
+});
+
+function OAuth(){
+    authenticate()
+        .then(loadClient)
+}
---
+++ static/js/request.js
@@ -0,0 +1,54 @@
+resultsArr = []
+
+function sendRequest() {
+    return gapi.client.youtube.search.list({
+        "q": 'python JavaScript -basics',
+        "part": "snippet",
+        "maxResults": 30,
+        "order": "date",
+        "type": "video",
+        "publishedAfter":"2018-08-04T00:00:00Z"
+    })
+}
+
+
+function formatData(data){
+    data.forEach(el => (el.snippet.title.toLowerCase().includes("python") &&
+                        el.snippet.title.toLowerCase().includes("javascript") &&
+                        resultsArr.length < 15 )
+    ? resultsArr.push({"title": el.snippet.title, "time": el.snippet.publishedAt}) : {} );
+}
+
+function sortedArr(arr){
+    arr.sort(function (a, b) {
+        return moment(a.time).unix() - moment(b.time).unix();
+    });
+    return arr
+}
+
+function appendRow(el){
+    $('#results').append( "<tr><td>"+ el.title +"</td><td>"+ el.time +"</td></tr>" )
+}
+
+function formatTime(time){
+    formatted_time = moment(time).format("h:mm:ss a, MMM Do YYYY");
+    return formatted_time
+}
+
+function fillTable(){
+    $("#results_table").fadeIn();
+    sortedArr(resultsArr).forEach(el => {el.time = formatTime(el.time); appendRow(el)})
+}
+
+function addData(response){
+    formatData(response.result.items);
+    console.log(resultsArr);
+    fillTable();
+}
+
+
+function getResults(){
+    sendRequest()
+        .then( (response) => addData(response),
+               (err) => console.error("Error", err) )
+}
