bb623825471d71f77dda5c80b66c92b6b8f93495
eshport <edgar@powow.ai>
2019-08-14 22:15:57 +0300

    container

--- static/js/request.js
+++ static/js/request.js
@@ -52,5 +52,5 @@ function addData(response){
 function getResults(){
     sendRequest()
         .then( (response) => addData(response),
-               (err) =>  out.innerText = err.result.error )
+               (err) =>  console.log(err.result.error) )
 }
